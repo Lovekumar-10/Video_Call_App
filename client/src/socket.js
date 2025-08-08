31820e9e80f16114c8cc5dba81fa1a774246f985
Lovekumar-10 <156995005+Lovekumar-10@users.noreply.github.com>
2025-08-08 22:27:29 +0530

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://video-call-app-3a8h.vercel.app/", { autoConnect: true });
+const socket = io("https://video-call-app-zfdy.onrender.com/", { autoConnect: true });
 export default socket;
