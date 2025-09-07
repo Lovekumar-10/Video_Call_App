f132c916e30e2f256e3b00d7098e23f0095b7608
Lovekumar-10 <156995005+Lovekumar-10@users.noreply.github.com>
2025-09-07 11:20:10 +0530

    App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,36 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import React, { useEffect } from "react";
+import ReactGA from "react-ga4";   // Google Analytics
 import Home from "./pages/Home"; 
 import Room from "./pages/Room"; 
 import Leave from "./pages/Leave";
 
 
+ReactGA.initialize("G-SQ58ZBDWNC");
 
 function App() {
+  const location = useLocation();
+
+
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: location.pathname });
+  }, [location]);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/room/:roomId" element={<Room />} />  
-        <Route path="/leave" element={<Leave />} /> 
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/room/:roomId" element={<Room />} />  
+      <Route path="/leave" element={<Leave />} /> 
+    </Routes>
   );
 }
 
-export default App;
+
+export default function WrappedApp() {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+}
